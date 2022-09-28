54b0d1a0dd1114ef2a3869f991199a7671efd60f
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-09-28 15:02:37 +0300

    Add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,15 @@
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description}) => 
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`
+).join('');
 
-console.log(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+let galleryClick = new SimpleLightbox('.gallery__item', {captionsData: "alt", captionDelay: 250});
