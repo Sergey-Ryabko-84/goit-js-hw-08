0b5a582bf6d9a791825a1880878e29b28e8a4515
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-09-30 18:42:10 +0300

    Add feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const save = (key, value) => {
+    try {
+        const serializedState = JSON.stringify(value);
+        localStorage.setItem(key, serializedState);
+    } catch (error) {
+        console.error("Set state error: ", error.message);
+    }
+};
+  
+const load = key => {
+    try {
+        const serializedState = localStorage.getItem(key);
+        return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (error) {
+        console.error("Get state error: ", error.message);
+    }
+};
+
+const LOCAL_STORAGE_KEY = "feedback-form-state";
+const formEl = document.querySelector('.feedback-form');
+
+function updateForm () {
+    if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
+        formEl.email.value = load(LOCAL_STORAGE_KEY).email;
+        formEl.message.value = load(LOCAL_STORAGE_KEY).message;
+        return;
+    }
+    formEl.email.value = '';
+    formEl.message.value = '';
+}
+
+function onInptForm () {
+    save(LOCAL_STORAGE_KEY, {email: formEl.email.value, message: formEl.message.value});
+}
+
+function onSubmitForm (e) {
+    e.preventDefault();
+    console.log(load(LOCAL_STORAGE_KEY));
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+    formEl.email.value = '';
+    formEl.message.value = '';
+}
+
+updateForm();
+formEl.addEventListener('input', throttle(onInptForm, wait=500));
+formEl.addEventListener('submit', onSubmitForm);
